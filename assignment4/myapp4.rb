86a6ece2c2f194f95823dcb8e5dea128f5ef91ef
RyotaHayashi <ryota.for.the.future@gmail.com>
2012-08-04 18:22:25 +0200

    Update assignment4/myapp4.rb

--- assignment4/myapp4.rb
+++ assignment4/myapp4.rb
@@ -5,21 +5,7 @@ require 'json'
 #require 'erb'
 #require 'sinatra'
 
-# Exchange your oauth_token and oauth_token_secret for an AccessToken instance.
-def prepare_access_token(oauth_token, oauth_token_secret)
-    consumer = OAuth::Consumer.new(
-	   "MPS14BRSSCdwA1JClJWQ",
-	   "8eaw5GQ1ESAPNzwDAASNotNNaHvhUjJjpvXKXsyjQCE",
-     { :site => "http://api.twitter.com",
-       :scheme => :header
-	 })
-  # now create the access token object from passed values
-  token_hash = { :oauth_token => "56079326-Xi8m7xg1lqDccKpO87Lx1GdrYTxwsY0kc1V1yHrB0",
-                 :oauth_token_secret => "x77ivBE6m0RZAu9Zx8RQzjLoRfOEDAPzLUzevgTrQQ"
-               }
-  access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
-  return access_token
-end
+
  
 # Exchange our oauth_token and oauth_token secret for the AccessToken instance.
 access_token = prepare_access_token("56079326-Xi8m7xg1lqDccKpO87Lx1GdrYTxwsY0kc1V1yHrB0", "x77ivBE6m0RZAu9Zx8RQzjLoRfOEDAPzLUzevgTrQQ")
